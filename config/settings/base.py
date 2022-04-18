919b34602308254fedc4019abd7a2ffe134a99d4
manish5798 <manish@darsa.ai>
2022-04-18 11:27:36 +0530

    Organization app created

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "its_tool.users",
+    "organization",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
