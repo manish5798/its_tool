84a6699e5bd5309de6baf758a3bd4de1e51fd1fb
manish5798 <manish@darsa.ai>
2022-04-18 12:57:48 +0530

    Registered models for admin site

--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from organization import models
 
 # Register your models here.
+
+admin.site.register(models.Entity)
+admin.site.register(models.Location)
+admin.site.register(models.Department)
+admin.site.register(models.Vendor)
+admin.site.register(models.Requests)
